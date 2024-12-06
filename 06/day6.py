63788d1716ed919fbbc5d8c3913e6f4109449234
Oscar Caballero <oscar.caballero@bayes.gg>
2024-12-06 09:52:14 +0100

    Multiprocess day 6

--- 06/day6.py
+++ 06/day6.py
@@ -2,6 +2,8 @@
 
 import sys
 import itertools
+import multiprocessing
+from functools import partial
 
 grid = {}
 
@@ -16,7 +18,7 @@ with open("input" if len(sys.argv) < 2 else sys.argv[1]) as f:
             grid[x, y] = char
 
 
-def move(pos, face):
+def move(grid, pos, face):
     x, y = pos
     movex, movey = face
     nxt = x + movex, y + movey
@@ -29,42 +31,45 @@ def move(pos, face):
             (0, 1): (-1, 0),
             (-1, 0): (0, -1),
         }.get(face)
-        return move(pos, face)
+        return move(grid, pos, face)
     else:
         assert grid[nxt] == ".", grid[nxt]
         return nxt, face
 
 
-def path(pos, face):
+def path(grid, pos, face):
     seen = set()
     while pos:
         seen.add(pos)
-        newpos, newface = move(pos, face)
+        newpos, newface = move(grid, pos, face)
         if newpos is None:
             return seen
         pos, face = newpos, newface
 
 
-def loops(pos, face):
+def loops(blockat, pos, face):
     seen = set()
+    newgrid = grid.copy()
+    newgrid[blockat] = '#'
     while pos:
         if (pos, face) in seen:
             return True
         seen.add((pos, face))
-        pos, face = move(pos, face)
-    if pos is None:
-        return False
+        pos, face = move(newgrid, pos, face)
 
-def blocktheguy(pos, face):
+    return False
+
+def p2(grid, pos, face):
     count = 0
-    spots = path(pos, face) - {pos}
-    for loc in spots:
-        grid[loc] = '#'
-        if loops(pos, face):
-            count += 1
-        grid[loc] = '.'
+    spots = path(grid, pos, face) - {pos}
+    # multiprocessing is not required here, but it does bring the runtime down from 10s to 2s
+    with multiprocessing.Pool() as pool:
+        func = partial(loops, pos=pos, face=face)
+        for res in pool.imap_unordered(func, spots):
+            count += res
+
     return count
 
 
-print("Part 1:", len(path(pos, face)))
-print("Part 2:", blocktheguy(pos, face))
+print("Part 1:", len(path(grid, pos, face)))
+print("Part 2:", p2(grid, pos, face))
